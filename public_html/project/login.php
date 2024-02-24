172a389c614f5b1846ca59b6d8946e9022acce22
jennatnguyen <curaroiky@gmail.com>
2024-02-24 03:08:03 -0500

    login part 2

--- public_html/project/login.php
+++ public_html/project/login.php
@@ -64,6 +64,8 @@
                    unset($user["password"]);
                    if (password_verify($password, $hash)) {
                        echo "Welcome $email";
+                       $_SESSION["user"] = $user;
+                       die(header("Location: home.php"));
                    } else {
                        echo "Invalid password";
                    }
