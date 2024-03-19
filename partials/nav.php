a3371fb73f1d2dcc2972db8703f0a4b0ecb95cb0
jennatnguyen <curaroiky@gmail.com>
2024-03-18 23:31:06 -0400

    UserProfile CORRECT

--- partials/nav.php
+++ partials/nav.php
@@ -11,7 +11,7 @@ $localWorks = true; //some people have issues with localhost for the cookie para
 if (($localWorks && $domain == "localhost") || $domain != "localhost") {
     session_set_cookie_params([
         "lifetime" => 60 * 60,
-        "path" => "/project",
+        "path" => "/Project",
         //"domain" => $_SERVER["HTTP_HOST"] || "localhost",
         "domain" => $domain,
         "secure" => true,
@@ -27,6 +27,7 @@ require_once(__DIR__ . "/../lib/functions.php");
     <ul>
         <?php if (is_logged_in()) : ?>
             <li><a href="home.php">Home</a></li>
+            <li><a href="profile.php">Profile</a></li>
         <?php endif; ?>
         <?php if (!is_logged_in()) : ?>
             <li><a href="login.php">Login</a></li>
