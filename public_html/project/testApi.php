48cb022c7dfcbf8d494d1d33df8997a0dffece62
MattToegel <Matthew.toegel@njit.edu>
2024-04-14 21:11:28 -0400

    Update testApi.php
    Example of extra headers

--- public_html/project/testApi.php
+++ public_html/project/testApi.php
@@ -8,7 +8,8 @@ if (isset($_GET["title"])) {
     $endpoint = "https://movies-tv-shows-database.p.rapidapi.com/";
     $isRapidAPI = true;
     $rapidAPIHost = "movies-tv-shows-database.p.rapidapi.com";
-    $result = get($endpoint, "SHOW_API_KEY", $data, $isRapidAPI, $rapidAPIHost);
+    $extra_headers["Type"] = "get-shows-by-title";
+    $result = get($endpoint, "SHOW_API_KEY", $data, $extra_headers, $isRapidAPI, $rapidAPIHost);
     //example of cached data to save the quotas, don't forget to comment out the get() if using the cached data for testing
     /* $result = ["status" => 200, "response" => '{
     "Global Quote": {
