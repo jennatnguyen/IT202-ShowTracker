93701825cf2f3a4b845fc4d31d3f2b21defcf97c
jennatnguyen <curaroiky@gmail.com>
2024-04-18 23:02:14 -0400

    database issue

--- lib/show_api.php
+++ lib/show_api.php
@@ -6,6 +6,20 @@ function fetch_show_by_title($title) {
     $rapidAPIHost = "movies-tv-shows-database.p.rapidapi.com";
     $extra_headers["Type"] = "get-shows-by-title";
     $result = get($endpoint, "SHOW_API_KEY", $data, $extra_headers, $isRapidAPI, $rapidAPIHost);
+
+    error_log("Response: " . var_export($result, true));
+    if (se($result, "status", 400, false) == 200 && isset($result["response"])) {
+        $result = json_decode($result["response"], true);
+        
+        if (isset($result["tv_results"])){
+            $result = $result["tv_results"];
+        }
+        else {
+            flash("Show does not exist");
+        }
+    } else {
+        $result = [];
+    }
     return $result;
 }
 ?>
